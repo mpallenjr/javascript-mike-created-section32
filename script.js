481e81c7ba4043005100245649021eeb595e3a65
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-21 16:01:11 -0600

    reserved words by javascript to avoid naming issues.

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ if (passTest) hasDriverLicense= true;
 if (hasDriversLicense) console.log('I can drive :D');
 //when strict mode is activates it throws this error (script.js:6 Uncaught ReferenceError: hasDriverLicense is not defined)
 //deactivated it thows no errors
+
+const interface = 'Audio'; // with strict mode (Uncaught SyntaxError: Unexpected strict mode reserved word)
+
+//strict mode reserves words JS may want to use in the future for a feature.  This helps avoid nameing conventions that can conflict witht eh language in the future.  
+
+//should alaways have strict mode on. 
